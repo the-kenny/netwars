a90b0fe41b67d6b8750163d61a3c250de65a3eb7
Moritz Ulrich <thekenny123@gmail.com>
2009-01-06 16:00:03 +0100

    Made a MapViewController-Class and used it
    (it does nothing at the moment)

---
+++ src/gui_cocoa/MapViewController.h
@@ -0,0 +1,17 @@
+//
+//  MapViewController.h
+//  gui_cocoa
+//
+//  Created by Moritz Ulrich on 06.01.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MapView.h"
+#import "game/scene.h"
+
+@interface MapViewController : NSViewController {
+
+}
+
+@end
