f6ef5765776b6f2431fd9ba90f16354d4f385df3
Moritz Ulrich <thekenny123@gmail.com>
2009-02-12 17:36:24 +0100

    Added terrain-drawing

--- src/gui_qt/MapWidget.cpp
+++ src/gui_qt/MapWidget.cpp
@@ -2,6 +2,10 @@
 
 #include <QPainter>
 
+#include "Sprites.h"
+
+#include "game/gui/drawing.h"
+
 MapWidget::MapWidget(QWidget* parent) 
 	: QWidget(parent), 
 	backgroundImage(":/data/background.png") {
@@ -12,4 +16,19 @@ void MapWidget::paintEvent(QPaintEvent* event) {
 	QPainter painter(this);
 
 	painter.drawImage(QPoint(), backgroundImage);
+
+	if(currentScene) {
+		this->drawTerrain(painter);
+	}
+}
+
+void MapWidget::drawTerrain(QPainter& painter) {
+	for(int x = 0; x < 30; ++x) {
+		for(int y = 0; y < 30; ++y) {
+			painter.drawImage(QPoint(x*16, y*16), 
+							  sharedSprites().getSprite(QString(aw::gui::get_sprite_for(
+									  					aw::coord(x, y), 
+														currentScene).c_str())));
+		}
+	}
 }
--- src/gui_qt/MapWidget.h
+++ src/gui_qt/MapWidget.h
@@ -4,17 +4,29 @@
 #include <QWidget>
 #include <QImage>
 
+#include "game/scene.h"
+
 class MapWidget: public QWidget {
 	Q_OBJECT
 
 	public:
 		MapWidget(QWidget* parent);
 
+		aw::scene::ptr scene() const { return currentScene; }
+		void setScene(aw::scene::ptr scene) { 
+			currentScene = scene; 
+			this->repaint(); 
+		}
+
 	protected:
 		virtual void paintEvent(QPaintEvent* event);
 
 	private:
+		void drawTerrain(QPainter& painter);
+
 		QImage backgroundImage;
+
+		aw::scene::ptr currentScene;
 };
 
 #endif
