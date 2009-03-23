285bff5e9997b7db086d9a9b50c0a5ac918752a3
Moritz Ulrich <thekenny123@gmail.com>
2009-03-23 01:08:00 +0100

    Extended make-qrc.lisp

--- src/gui_qt/make-qrc.lisp
+++ src/gui_qt/make-qrc.lisp
@@ -1,20 +1,24 @@
+;; make-qrc.lisp
+;; Generates the data.qrc file from the data/ directory.
+;; Run with sbcl --load make-qrc.lisp
+
 (require 'cl-who)
 (require 'cl-fad)
 
 (use-package '(:cl-who :cl-fad))
 
- #|
- <!DOCTYPE RCC>
- <RCC version="1.0">
- <qresource>
-     <file>images/copy.png</file>
-     <file>images/cut.png</file>
-     <file>images/new.png</file>
-     <file>images/open.png</file>
-     <file>images/paste.png</file>
-     <file>images/save.png</file>
- </qresource>
- </RCC>
+#|
+<!DOCTYPE RCC>
+<RCC version="1.0">
+  <qresource>
+    <file>images/copy.png</file>
+    <file>images/cut.png</file>
+    <file>images/new.png</file>
+    <file>images/open.png</file>
+    <file>images/paste.png</file>
+    <file>images/save.png</file>
+   </qresource>
+</RCC>
 |#
 
 (defun make-relative (path &optional (to *default-pathname-defaults*))
@@ -28,13 +32,36 @@
     :name (pathname-name path)
     :type (pathname-type path)))
 
+(defun replace-all (string part replacement &key (test #'char=))
+  "Returns a new string in which all the occurences of the part 
+is replaced with replacement."
+  (with-output-to-string (out)
+	(loop with part-length = (length part)
+            for old-pos = 0 then (+ pos part-length)
+	   for pos = (search part string
+						 :start2 old-pos
+						 :test test)
+	   do (write-string string out
+						:start old-pos
+						:end (or pos (length string)))
+	   when pos do (write-string replacement out)
+	   while pos)))
+
 
 (setf *prologue* "<!DOCTYPE RCC>")
-(with-open-file (file "data.qrc" :direction :output :if-exists :supersede)
-  (cl-who:with-html-output (*standard-output* file :prologue t :indent nil)
-                           (:RCC :version "1.0"
-                                 (:qresource
-                                   (walk-directory #P"data/" #'(lambda (file)
-                                                                 (htm (:file (str (namestring (make-relative file)))))))))))
 
-(print "You have to change rcc to RCC in data.qrc before it works!")
+(with-open-file (file "data.qrc" :direction :output :if-exists :supersede)
+  (write-string (replace-all
+				 (cl-who:with-html-output-to-string
+					 (*standard-output* nil :prologue t)
+				   (:RCC :version "1.0"
+						 (:qresource
+						  (walk-directory
+						   #P"data/"
+						   #'(lambda (file)
+							   (htm (:file (str
+											(namestring
+											 (make-relative file))))))))))
+				 "rcc"
+				 "RCC")
+				file)))
