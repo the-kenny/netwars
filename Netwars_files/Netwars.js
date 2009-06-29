8cf2fb80dfa84d938e16da8ba0f769a2e8e80cc6
Moritz Ulrich <thekenny123@gmail.com>
2009-06-29 13:23:31 +0200

    Removed the border around the screenshots

--- Netwars_files/Netwars.js
+++ Netwars_files/Netwars.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20090628
+// Created by iWeb 2.0.4 local-build-20090629
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.60,offset:1.00}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
--- Screenshots_files/Screenshots.js
+++ Screenshots_files/Screenshots.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20090628
+// Created by iWeb 2.0.4 local-build-20090629
 
 function createMediaStream_id2()
 {return IWCreatePhotocast("http://the-kenny.ath.cx/%7EMoritz/netwars/Netwars/Screenshots_files/rss.xml",true,true);}
@@ -9,7 +9,7 @@ function layoutMediaGrid_id2(range)
 {createMediaStream_id2().load('http://the-kenny.ath.cx/%7EMoritz/netwars/Netwars',function(imageStream)
 {if(range==null)
 {range=new IWRange(0,imageStream.length);}
-IWLayoutPhotoGrid('id2',new IWPhotoGridLayout(4,new IWSize(140,140),new IWSize(140,32),new IWSize(172,187),27,27,0,new IWSize(12,12)),new IWPhotoFrame([IWCreateImage('Screenshots_files/Formal_inset_01.png'),IWCreateImage('Screenshots_files/Formal_inset_02.png'),IWCreateImage('Screenshots_files/Formal_inset_03.png'),IWCreateImage('Screenshots_files/Formal_inset_06.png'),IWCreateImage('Screenshots_files/Formal_inset_09.png'),IWCreateImage('Screenshots_files/Formal_inset_08.png'),IWCreateImage('Screenshots_files/Formal_inset_07.png'),IWCreateImage('Screenshots_files/Formal_inset_04.png')],null,0,0.450000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000),imageStream,range,null,null,1.000000,{backgroundColor:'#000000',reflectionHeight:100,reflectionOffset:2,captionHeight:100,fullScreen:0,transitionIndex:2},'Media/slideshow.html','widget1','widget2','widget3')});}
+IWLayoutPhotoGrid('id2',new IWPhotoGridLayout(4,new IWSize(129,129),new IWSize(129,32),new IWSize(159,176),27,27,0,new IWSize(2,2)),new IWEmptyStroke(),imageStream,range,null,null,1.000000,{backgroundColor:'#000000',reflectionHeight:100,reflectionOffset:2,captionHeight:100,fullScreen:0,transitionIndex:2},'Media/slideshow.html','widget1','widget2','widget3')});}
 function relayoutMediaGrid_id2(notification)
 {var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id2(range);}
 function onStubPage()
