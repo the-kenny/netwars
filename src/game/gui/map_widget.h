7f1da40d3a75575e4612ae5d987a714dd1655de2
Moritz Ulrich <thekenny123@gmail.com>
2009-09-29 17:39:24 +0200

    Used boost::signals2 in the game-core.

--- src/game/gui/map_widget.h
+++ src/game/gui/map_widget.h
@@ -1,47 +1,44 @@
 #ifndef AW_DISPLAY_H
-	#define AW_DISPLAY_H
+#define AW_DISPLAY_H
 
-#include <boost/signals.hpp>
+#include <boost/signals2/signal.hpp>
 
 #include "game/scene.h"
 
 namespace aw
 {
-	namespace gui
+  namespace gui
+  {
+	class map_widget
 	{
-		class map_widget
-		{
-			public:
-//				typedef boost::signal<void (int, int, int)> signal_clicked_t;
-//				typedef boost::signal<void (int, int)> signal_focus_changed_t;
+	public:
+	  typedef boost::signals2::signal<void 
+									  (const coord &, int)> signal_clicked_t;
+	  typedef boost::signals2::signal<void 
+									  (const coord &)> signal_focus_changed_t;
+	
+	  typedef boost::shared_ptr<map_widget> ptr;
 
-				typedef boost::signal<void (const coord &, int)> signal_clicked_t;
-				typedef boost::signal<void (const coord &)> signal_focus_changed_t;
+	  map_widget();
+	  virtual ~map_widget() {}
+	  void display(const scene::ptr &p);
+	  void reset();
 
-				typedef boost::shared_ptr<map_widget> ptr;
-
-				map_widget();
-				virtual ~map_widget() {}
-				void display(const scene::ptr &p);
-				void reset();
-
-				void write_to_png(const std::string &filename);
-				virtual void queue_draw() = 0;
+	  void write_to_png(const std::string &filename);
+	  virtual void queue_draw() = 0;
 				
-				virtual void disable() = 0;
-				virtual void enable() = 0;
+	  virtual void disable() = 0;
+	  virtual void enable() = 0;
 
-				signal_clicked_t &on_click() { return m_signal_click; }
-				signal_focus_changed_t &on_focus_changed() { return m_signal_focus_changed; }
+	  signal_clicked_t &on_click() { return m_signal_click; }
+	  signal_focus_changed_t &on_focus_changed() { return m_signal_focus_changed; }
 
-			protected:
-				scene::ptr m_scene;
-				signal_clicked_t m_signal_click;
-				signal_focus_changed_t m_signal_focus_changed;
-		};
-
-		//bool is_connectable(terrain::types lhs, terrain::types rhs);
-	}
+	protected:
+	  scene::ptr m_scene;
+	  signal_clicked_t m_signal_click;
+	  signal_focus_changed_t m_signal_focus_changed;
+	};
+  }
 }
 
 #endif
